8bc15d2e52e9f2ddc769b92b2b3481403c062b5b
Alexander Ingles <aingles@my.bcit.ca>
2025-03-04 22:04:23 -0800

    Commit and push at assignment step 4.4

--- scripts/firebaseAPI_template.js
+++ scripts/firebaseAPI_template.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyAFEolJSaITTJH2AVU - DxbGSapXdOl1icg",
+    authDomain: "bcitprojectalex.firebaseapp.com",
+    projectId: "bcitprojectalex",
+    storageBucket: "bcitprojectalex.firebasestorage.app",
+    messagingSenderId: "520119185958",
+    appId: "1:520119185958:web:cad88862c2ebfa1fc7ace7"
 };
 
 //--------------------------------------------
